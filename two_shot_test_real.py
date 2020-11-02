b0861ceb2264775567a9089014680f7af8cc3589
Darren Flaks <darrenf0209@gmail.com>
2020-11-02 12:55:02 +1100

    two shot test is working!

--- two_shot_test_real.py
+++ two_shot_test_real.py
@@ -2,23 +2,32 @@ import os
 import torch
 import torchvision
 import torchvision.transforms as T
+import torch.nn as nn
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+import matplotlib.pyplot as plt
 
-from data.face_detection_data_loader import face_classes
+# from data.face_detection_data_loader import face_classes
+from data.face_classification_data_loader import face_classes
+
 
 SAVED_MODEL_DIR = "saved_models"
 SAVED_DET_NET = "faceDetectorBest.pt"
-SAVED_CLA_NET = "faceClassificationBest.pt"
+SAVED_CLA_NET = "Face_Classification_SGD_StepLR_10_25_0.005_best.pt"
 IMAGES_DIR = "data/darren_test/"
 SAVE_OUTPUTS_DIR = "results/model_outputs/"
+
+if not os.path.exists(os.path.join(SAVE_OUTPUTS_DIR,"TWO_SHOT_TEST")):
+    os.makedirs(os.path.join(SAVE_OUTPUTS_DIR,"TWO_SHOT_TEST"))
+
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Faster RCNN Model - pretrained on COCO - detection net
 detectionNet = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
-num_classes = len(face_classes) # Should be 2 - backround and face
+# num_classes = len(face_classes) # Should be 2 - backround and face
+num_classes = 5
 in_features = detectionNet.roi_heads.box_predictor.cls_score.in_features
 detectionNet.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
@@ -29,52 +38,68 @@ classificationNet.fc = nn.Linear(512, 4) # Overwrite clasification layer from Im
 # Load Models
 detectionNet.load_state_dict(torch.load(os.path.join(SAVED_MODEL_DIR, SAVED_DET_NET)))
 classificationNet.load_state_dict(torch.load(os.path.join(SAVED_MODEL_DIR, SAVED_CLA_NET)))
+detectionNet.eval().to(device)
+classificationNet.eval().to(device)
 
 # Load Test Data
 image_names = os.listdir(IMAGES_DIR)
 
-# Get outputs of model
-model.to(device).eval()
-for idx in range(len(image_names)):
-    image_path = os.path.join(IMAGES_DIR, image_names[idx])
-    image = Image.open(image_path)
-    image = np.array(image) / 255.0
-    image = torch.tensor(image, dtype=torch.float).permute(2,0,1).contiguous()
-    image = torch.unsqueeze(image, dim=0)
-    
-    # Pass entire image through face detetion network
-    out = model(image.to(device))
+# ClassificationNet Transforms
+transforms = T.Compose([
+    T.ToPILImage(),
+    T.Resize((128, 128)),
+    T.ToTensor()
+])
+with torch.no_grad():
+    # Get outputs of model
+    # model.to(device).eval()
+    for idx in range(len(image_names)):
+        image_path = os.path.join(IMAGES_DIR, image_names[idx])
+        image = Image.open(image_path)
+        image = np.array(image) / 255.0
+        image = torch.tensor(image, dtype=torch.float).permute(2,0,1).contiguous()
+        image = torch.unsqueeze(image, dim=0)
 
-    boxes = out[0]['boxes'].cpu().detach()
-    labels = out[0]['labels'].cpu().detach()
-    scores = out[0]['scores'].cpu().detach()
-    keep = torchvision.ops.nms(boxes=boxes, scores=scores, iou_threshold=0.3)
+        # Pass entire image through face detetion network
+        out = detectionNet(image.to(device))
 
-    image = image[0]
-    image = np.moveaxis(image.numpy(),0,2)
-    image = 255 * image
-    image = image.astype(np.uint8)
-    image = T.ToPILImage()(image)
-    img1 = ImageDraw.Draw(image)
+        boxes = out[0]['boxes'].cpu().detach()
+        labels = out[0]['labels'].cpu().detach()
+        scores = out[0]['scores'].cpu().detach()
+        keep = torchvision.ops.nms(boxes=boxes, scores=scores, iou_threshold=0.3)
 
-    for i in range(len(keep)):
-        index = keep[i]
-        box = boxes[index].numpy()
-        x1 = box[0]
-        y1 = box[1]
-        x2 = box[2]
-        y2 = box[3]
+        image_to_draw = image[0]
+        image_to_draw = np.moveaxis(image_to_draw.numpy(),0,2)
+        image_to_draw = 255 * image_to_draw
+        image_to_draw = image_to_draw.astype(np.uint8)
+        image_to_draw = T.ToPILImage()(image_to_draw)
+        img1 = ImageDraw.Draw(image_to_draw)
 
-        # Pass each bounding box crop through the classification network to get label
-        image_crop = image[y1:y2+1, x1:x2+1, :] # H, W, C
-        label = torch.argmax(classificationNet(image_crop))
+        for i in range(len(keep)):
+            index = keep[i]
+            box = boxes[index].numpy()
+            x1 = int(round(box[0]))
+            y1 = int(round(box[1]))
+            x2 = int(round(box[2]))
+            y2 = int(round(box[3]))
 
-        score = scores[index].numpy()
 
-        font_size = 40
-        font = ImageFont.truetype("arial.ttf", font_size)
-        img1.rectangle(box, outline ="red")
-        img1.text([box[0], box[1]], face_classes[label], font=font)
+            # Pass each bounding box crop through the classification network to get label
+            # image = torch.squeeze(image, dim=0)
 
-    #image.show() 
-    image.save(SAVE_OUTPUTS_DIR + f"realTest{idx}.jpg")
+            image_crop = image[:, :, y1:y2+1, x1:x2+1] # B, C, H, W
+            image_crop = image_crop[0, :, :, :]
+            image_crop = transforms(image_crop)
+            image_crop = torch.unsqueeze(image_crop, dim=0).to(device)
+            label = torch.argmax(classificationNet(image_crop))
+
+            score = scores[index].numpy()
+
+            font_size = 40
+            font = ImageFont.truetype("arial.ttf", font_size)
+
+            img1.rectangle(box, outline ="red")
+            img1.text([box[0], box[1]], face_classes[label], font=font)
+
+        # image_to_draw.show()
+        image_to_draw.save(os.path.join(SAVE_OUTPUTS_DIR,"TWO_SHOT_TEST", f"realTest{idx}.jpg"))
